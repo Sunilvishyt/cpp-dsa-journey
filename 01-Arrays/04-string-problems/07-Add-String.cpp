0de4572a170c36f54f3f9be42e17b1269a52d157
Sunil Vishwakarma <97221099+Sunilvishyt@users.noreply.github.com>
2026-03-20 22:36:54 +0530

    Add solution to sum two strings (numbers)
    Refactor addfn to ensure num1 is always larger and improve comments.

--- 01-Arrays/04-string-problems/07-Add-String.cpp
+++ 01-Arrays/04-string-problems/07-Add-String.cpp
@@ -8,14 +8,17 @@
  * Space Complexity: O(n) //because using extra string for storing ans.
 */
 
+
 class Solution {
 public:
+    //create a function so we can control that num1 will always be the largest in terms of size.
     string addfn(string num1, string num2){
-        int firstidx = num1.size() -1;
-        int secidx = num2.size() -1;
+        int firstidx = num1.size() -1; //handle num1
+        int secidx = num2.size() -1;  //handle num2
         int carry = 0;
         string ans = "";
-
+        
+        //add all numbers from num2.
         while ( secidx >= 0 ){
             int sum = (num1[firstidx] - '0') + (num2[secidx] - '0') + carry;
             carry = sum / 10;
@@ -24,6 +27,7 @@ public:
             firstidx--; secidx--;
         }
 
+        //add the numbers that were remain in num1 to the ans and also add carry in them.
         while ( firstidx >= 0 ) {
             int sum = (num1[firstidx] - '0') + carry;
             carry = sum / 10;
@@ -32,6 +36,7 @@ public:
             firstidx--;
         }
 
+        //if carry still remain at last so we have to add it to the ans.
         if (carry > 0)
             ans += (carry + '0');
         
@@ -40,6 +45,7 @@ public:
     }
 
     string addStrings(string num1, string num2) {
+        //send in the form where num1 if always the largest one.
         if (num1.size() >= num2.size())
             return addfn(num1,num2);
 
