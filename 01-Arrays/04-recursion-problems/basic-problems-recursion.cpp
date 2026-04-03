3c6834c79dad3952296725505eb4877ffd251a0e
Sunil Vishwakarma <97221099+Sunilvishyt@users.noreply.github.com>
2026-04-03 12:57:27 +0530

    Solved more multiple recursion problems

--- 01-Arrays/04-recursion-problems/basic-problems-recursion.cpp
+++ 01-Arrays/04-recursion-problems/basic-problems-recursion.cpp
@@ -1,4 +1,4 @@
-//////////////////printing array elements with recursion \\\\\\\\\\\\\\\\\
+//////////////////////////printing array elements with recursion \\\\\\\\\\\\\\\\\\\\\\\\\\
 
 #include <iostream>
 using namespace std;
@@ -19,7 +19,7 @@ int main()
 }
 
 
-//////////////sum of arrays with recursion\\\\\\\\\\\\\\
+/////////////////////////////sum of arrays with recursion\\\\\\\\\\\\\\\\\\\\\\\\\\\
 #include <iostream>
 using namespace std;
 
@@ -38,7 +38,7 @@ int main()
 }
 
 
-/////////////minimum element in array with recursion\\\\\\\\\\\\\\\\\\\\\
+///////////////////////////minimum element in array with recursion\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 #include <iostream>
 using namespace std;
 
@@ -53,6 +53,77 @@ int main()
 {
     int arr[] = {1,2,3,4,-4,6};
     cout << minelement(arr, sizeof(arr) / sizeof(arr[0])-1, 0);
+    return 0;
+}
+
+////////////////////////////check palindrome with recursion\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+#include <iostream>
+using namespace std;
+
+bool checkpal(const string &str, int start, int end){
+    if (start >= end) 
+        return true;
+        
+    if (str[start] != str[end]) 
+        return false;
+        
+    return checkpal(str, start+1, end-1);
+}
+
+int main()
+{    
+    string str = "racecar";
+    if (checkpal(str, 0, str.size()-1)) 
+        cout << "given string is palindrome";
+        
+    else 
+        cout << "not palindrome";
+        
+    return 0;
+}
+
+///////////////////////////////check vowel or not with recursion\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+#include <iostream>
+using namespace std;
+
+int checkvow(const string &str, int end){
+    if (end == -1) 
+        return 0;
+        
+    if (str[end] == 'a' or str[end] == 'e' or str[end] == 'i' or str[end] == 'o' or str[end] == 'u')
+        return 1 + checkvow(str , end -1);
+        
+    else return checkvow(str, end -1);
+}
+
+int main()
+{    
+    string str = "aeioudfdfgg";
+    int ans = checkvow(str, str.size()-1);
+    cout << ans ;
+    return 0;
+}
+
+
+//////////////reverse string with recursion.\\\\\\\\\\\\\\\\\\\\\\
+#include <iostream>
+using namespace std;
+
+void reversestr(string &str, int start, int end){
+    if (start >= end) 
+        return;
+        
+    swap(str[start], str[end]);
+    reversestr(str, start + 1, end -1);
+    return;
+}
+
+int main()
+{    
+    string str = "aaabbbb";
+    reversestr(str, 0, str.size()-1);
+    
+    cout << str ;
     return 0;
 }
 
