2e15304e633c79dd8630427c3459d9f9dddcb7d0
Sunil Vishwakarma <97221099+Sunilvishyt@users.noreply.github.com>
2026-04-18 12:30:11 +0530

    Implement optimized permutation solution
    Added an optimized version of the permutation function that uses minimal space.

--- 01-Arrays/04-recursion-problems/07-permutations.cpp
+++ 01-Arrays/04-recursion-problems/07-permutations.cpp
@@ -44,3 +44,48 @@ int main()
 
     return 0;
 }
+
+
+// ///////////////////////////optimized version/////////////////////////////
+//using least space as possible
+
+
+#include <print>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void permute(vector<int>&arr, vector<vector<int>>&ans, int index){
+  //base condition
+  if (index == arr.size()){
+    ans.push_back(arr);
+    return;
+  }
+  
+  for (int i = index; i < arr.size(); i++){
+    swap(arr[i], arr[index]);
+
+    permute(arr, ans, index+1);
+
+    swap(arr[i], arr[index]);
+
+  }
+}
+
+int main()
+{
+    vector<int>arr = {1,2,3};
+    vector<vector<int>>ans;
+    int index = 0;
+    
+    permute(arr, ans, index);
+    
+    for (int i = 0; i < ans.size(); i++){
+        for (int j = 0; j < ans[i].size(); j++){
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
